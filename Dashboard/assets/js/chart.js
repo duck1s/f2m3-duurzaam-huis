7759d94fa38ce4e59ea94cca906fb894698ab956
Duck1s <joeri.breedveld123@gmail.com>
2022-04-07 21:47:08 +0200

    paar charts er in gezet

---
+++ Dashboard/assets/js/chart.js
@@ -0,0 +1,58 @@
+var stroomValues = [8599, 8626, 8637, 8644, 8654, 8679, 8689];
+var dates = [
+	"04-03-2022",
+	"09-03-2022",
+	"10-03-2022",
+	"12-03-2022",
+	"14-03-2022",
+	"16-03-2022",
+	"17-03-2022",
+];
+var stroomChart = document.getElementById("stroomChart");
+
+var stroomChart = new Chart(stroomChart, {
+	type: "line",
+	data: {
+		labels: dates,
+		datasets: [
+			{
+				label: "Stroommeter",
+				data: stroomValues,
+				borderRadius: 10,
+				borderWidth: 1,
+				fill: true,
+				responsive: true,
+			},
+		],
+	},
+});
+
+var waterValues = [1204.614, 1205.320, 1205.703, 1206.714, 1207.551, 1208.153, 1208.387];
+var dates = [
+	"04-03-2022",
+	"09-03-2022",
+	"10-03-2022",
+	"12-03-2022",
+	"14-03-2022",
+	"16-03-2022",
+	"17-03-2022",
+];
+var waterChart = document.getElementById("waterChart");
+
+var waterChart = new Chart(waterChart, {
+	type: "line",
+	data: {
+		labels: dates,
+		datasets: [
+			{
+				label: "Watermeter",
+				data: waterValues,
+				borderRadius: 10,
+				borderWidth: 1,
+				fill: true,
+				responsive: true,
+			},
+		],
+	},
+});
+
