a9f8e3863935834a94bcfc4c6c6ecab87e8b80a6
Duck1s <joeri.breedveld123@gmail.com>
2022-04-07 21:31:20 +0200

    add: time 24uur klok

--- Dashboard/assets/js/main.js
+++ Dashboard/assets/js/main.js
@@ -29,13 +29,6 @@ const getCurrentTimeDate = () => {
 	var minutes = currentTimeDate.getMinutes();
 	minutes = minutes < 10 ? "0" + minutes : minutes;
 
-	var AMPM = hours >= 12 ? "PM" : "AM";
-
-	if (hours === 12) {
-		hours = 12;
-	} else {
-		hours = hours % 12;
-	}
 
 	var currentTime = `${hours}:${minutes}`;
 	var currentDay = weekday[currentTimeDate.getDay()];
